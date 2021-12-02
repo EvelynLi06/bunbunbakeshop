47d3a3a6d88f3904ee83041247d7a8fd5b61b192
Evelyn Li <chenxuli@andrew.cmu.edu>
2021-12-02 15:14:45 -0500

    Popout window task adding bug fixed

--- Chrome Extension/loadPopup.js
+++ Chrome Extension/loadPopup.js
@@ -1,2 +1,8 @@
 fetchTodos();
-addEventListenerToAdd("todo");
+const input = document.getElementById("add-input");
+input.replaceWith(input.cloneNode(true));
+document.getElementById("add-input").addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        addTodo();
+    }
+});
